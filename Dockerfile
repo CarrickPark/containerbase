c780518586df6096b9a146d7e62d4a1978c3a618
Rai Phairow <rai.phairow@github.com>
2017-03-17 16:48:58 -0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM debian:jessie
+FROM debian:8.7
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
         unzip \
     && rm -rf /var/lib/apt/lists/*
 
-ENV CONSUL_VERSION 0.6.3
-ENV CONSUL_SHA256 b0532c61fec4a4f6d130c893fd8954ec007a6ad93effbe283a39224ed237e250
+ENV CONSUL_VERSION 0.7.3
+ENV CONSUL_SHA256 901a3796b645c3ce3853d5160080217a10ad8d9bd8356d0b73fcd6bc078b7f82
 
 ADD https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip /tmp/consul.zip
 RUN echo "${CONSUL_SHA256}  /tmp/consul.zip" > /tmp/consul.sha256 \
@@ -14,6 +14,17 @@ RUN echo "${CONSUL_SHA256}  /tmp/consul.zip" > /tmp/consul.sha256 \
     && unzip /tmp/consul.zip \
     && chmod +x /bin/consul \
     && rm /tmp/consul.zip
+
+ENV CONSUL_TEMPLATE_VERSION 0.18.1
+ENV CONSUL_TEMPLATE_SHA256 99dcee0ea187c74d762c5f8f6ceaa3825e1e1d4df6c0b0b5b38f9bcb0c80e5c8
+
+ADD https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip /tmp/consul-template.zip
+RUN echo "${CONSUL_TEMPLATE_SHA256}  /tmp/consul-template.zip" > /tmp/consul-template.sha256 \
+    && sha256sum -c /tmp/consul-template.sha256 \
+    && cd /bin \
+    && unzip /tmp/consul-template.zip \
+    && chmod +x /bin/consul-template \
+    && rm /tmp/consul-template.zip
 
 EXPOSE 8300 8301 8301/udp 8302 8302/udp 8400 8500 8600 8600/udp
 ENV DNS_RESOLVES consul
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "${1:0:1}" = '-' ]; then
 	# command starts with dash so it must be options for consul agent
+
 	if [ "$CP_ORCHESTRATOR_PRIMARY_IP" != "" ]; then
 		# when primary ip is present then we need to join primary
 		set -- /bin/consul agent -config-dir=/config -join=$CP_ORCHESTRATOR_PRIMARY_IP -encrypt=$CP_ORCHESTRATOR_ENCRYPT "$@"
@@ -10,14 +11,57 @@ if [ "${1:0:1}" = '-' ]; then
 		# when no primary ip is present then we are primary
 		set -- /bin/consul agent -config-dir=/config -encrypt=$CP_ORCHESTRATOR_ENCRYPT "$@"
 	fi
+
+	EXEC_COMMAND="$@"
 else
-    # run consul agent in addition to the command specified
-	AGENT="/bin/consul agent -config-dir=/config -join=$CP_ORCHESTRATOR_PRIMARY_IP -encrypt=$CP_ORCHESTRATOR_ENCRYPT"
+    # run consul agent and consul template in addition to the command specified
+	AGENT="/bin/consul agent -config-dir/config -join=$CP_ORCHESTRATOR_PRIMARY_IP -encrypt=$CP_ORCHESTRATOR_ENCRYPT"
 	#echo Running Agent: "$AGENT"
-	
-	# run in background
+
+	# run agent in background
 	/bin/bash -c "$AGENT" &
+
+	EXEC_COMMAND="$@"
 fi
 
-#echo Running Command: "$@"
-exec "$@"
+## support up to 6 templated files or just execute the command directly
+if [ -n "$CP_TEMPLATE_6" ]; then
+	/bin/consul-template \
+	  -template="${CP_TEMPLATE}" \
+		-template="${CP_TEMPLATE_2}" \
+		-template="${CP_TEMPLATE_3}" \
+		-template="${CP_TEMPLATE_4}" \
+		-template="${CP_TEMPLATE_5}" \
+		-template="${CP_TEMPLATE_6}" \
+		-exec="$EXEC_COMMAND"
+elif [ -n "$CP_TEMPLATE_5" ]; then
+	/bin/consul-template \
+	  -template="${CP_TEMPLATE}" \
+		-template="${CP_TEMPLATE_2}" \
+		-template="${CP_TEMPLATE_3}" \
+		-template="${CP_TEMPLATE_4}" \
+		-template="${CP_TEMPLATE_5}" \
+		-exec="$EXEC_COMMAND"
+elif [ -n "$CP_TEMPLATE_4" ]; then
+	/bin/consul-template \
+	  -template="${CP_TEMPLATE}" \
+		-template="${CP_TEMPLATE_2}" \
+		-template="${CP_TEMPLATE_3}" \
+		-template="${CP_TEMPLATE_4}" \
+		-exec="$EXEC_COMMAND"
+elif [ -n "$CP_TEMPLATE_3" ]; then
+	/bin/consul-template \
+	  -template="${CP_TEMPLATE}" \
+		-template="${CP_TEMPLATE_2}" \
+		-template="${CP_TEMPLATE_3}" \
+		-exec="$EXEC_COMMAND"
+elif [ -n "$CP_TEMPLATE_2" ]; then
+	/bin/consul-template \
+	  -template="${CP_TEMPLATE}" \
+		-template="${CP_TEMPLATE_2}" \
+		-exec="$EXEC_COMMAND"
+elif [ -n "$CP_TEMPLATE" ]; then
+	/bin/consul-template -template="${CP_TEMPLATE}" -exec="$EXEC_COMMAND"
+else
+	exec $EXEC_COMMAND
+fi
